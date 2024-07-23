3e2bb55d456dc4f0d7b8ed48ec18bd0cd6d1fa3d
Lumine_ <grace.estrada@ftwfoundation.org>
2024-07-23 14:18:47 +0800

    Added pytest, __init__.py, and test_baccarat.py

---
+++ game-server/tests/__init__.py
@@ -0,0 +1,2 @@
+from games.baccarat.helper_functions import build_deck, rebuild_deck, build_combinations, announce_winner, announce_bet_winner, compute_payout, play_game
+from games.baccarat.sim import simulate_baccarat
