ad9aabcb13b02f37a974fa65d43964904ef2e592
markcabalona <schmosby@LAPTOP-HNOVMVCO.localdomain>
2024-07-31 23:08:43 +0800

    feature: implemented different types of RouletteBet

--- game-server/games/roulette/bet/__init__.py
+++ game-server/games/roulette/bet/__init__.py
@@ -1,3 +1,20 @@
 from .roulette_bet import RouletteBet
-from .roulette_bet_type import RouletteBetType, EighteenNumberBetType, ColumnBetType, DozenBetType
+from .roulette_bet_type import (
+    RouletteBetType,
+    EighteenNumberBetType,
+    ColumnBetType,
+    DozenBetType,
+)
 
+from .roulette_bet_impl import (
+    StraightUpBet,
+    SplitBet,
+    StreetBet,
+    FiveNumberBet,
+    LineBet,
+    DozenBet,
+    ColumnBet,
+    EighteenNumberBet,
+    ColorBet,
+    OddOrEvenBet,
+)
