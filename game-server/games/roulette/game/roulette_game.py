b5cfcb61d699a2fb108ca29cc988d0132895fb2b
markcabalona <schmosby@LAPTOP-HNOVMVCO.localdomain>
2024-08-03 15:24:10 +0800

    feature: update data type of RouletteGame.__winning_pocket
    - update __winning_pocket data type to WiningPocket

--- game-server/games/roulette/game/roulette_game.py
+++ game-server/games/roulette/game/roulette_game.py
@@ -1,12 +1,12 @@
 from random import Random
 from typing import List
-from .roulette_pocket import RoulettePocket, PocketColor
+from .roulette_pocket import RoulettePocket, PocketColor, WinningPocket
 from ..bet.roulette_bet import RouletteBet
 
 
 class RouletteGame:
     __pockets = List[RoulettePocket]
-    __winning_pocket: RoulettePocket = None
+    __winning_pocket: WinningPocket = None
     __bets: List[RouletteBet] = []
 
     def __init__(self):
@@ -15,14 +15,14 @@ class RouletteGame:
         self.__bets = []
 
     def spin(self):
-        self.__winning_pocket = self.pocket_from_pocket_number(
-            pocket_number=Random().randint(-1, 36)
+        self.__winning_pocket = WinningPocket(
+            pocket_number=Random().randint(-1, 36),
         )
 
         for bet in self.__bets:
             bet.compute_winnings(self.__winning_pocket)
 
-    def get_winning_pocket(self) -> RoulettePocket:
+    def get_winning_pocket(self) -> WinningPocket:
         return self.__winning_pocket
 
     def get_winning_bets(self) -> List[RouletteBet]:
