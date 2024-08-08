08c3581d819810cdabdc4600da8ba2a9049c16a6
markcabalona <markcabalona58@gmail.com>
2024-08-09 00:18:32 +0800

    blackjack-etl: implement extract fro blackjack game
    - send GET request to /blackjack endpoint
    - convert response to BlackJackResponse datamodel of request is successful
    - raise an exception otherwise

---
+++ app-server/games/blackjack/extract.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from requests import get, Response
+from models import BlackJackResponse
+
+
+def extract(
+    players: Optional[int] = None,
+    games: Optional[int] = None,
+) -> BlackJackResponse:
+    base_url: str = "http://127.0.0.1:8000"
+    path: str = "/blackjack"
+
+    url = base_url + path
+
+    response: Response = get(
+        url,
+        params={
+            "players": players,
+            "games": games,
+        },
+    )
+
+    if response.ok:
+        return BlackJackResponse(**response.json())
+    else:
+        raise Exception(response.json())
