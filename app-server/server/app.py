f22f4449e00558d93396cb36a7eb9e0ae102d582
markcabalona <markcabalona58@gmail.com>
2024-08-09 00:29:12 +0800

    blackjack-etl: invoke blackjack_pipeline in server.app

--- app-server/server/app.py
+++ app-server/server/app.py
@@ -3,10 +3,13 @@ import sys
 import pandas as pd
 
 # Add the parent directory of 'games' (i.e., 'app-server') to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # Import modules
-from games.baccarat import pipeline_baccarat 
+from games.baccarat import pipeline_baccarat
+from games.blackjack import blackjack_pipeline
 
 
 pipeline_baccarat()
+
+blackjack_pipeline()
