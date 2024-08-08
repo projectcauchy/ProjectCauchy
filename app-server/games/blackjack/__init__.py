90be746456bc6aebcf50adfbcbe46f7fc0f385ac
markcabalona <markcabalona58@gmail.com>
2024-08-09 00:28:25 +0800

    blackjack-etl: implement blackjack_pipeline

---
+++ app-server/games/blackjack/__init__.py
@@ -0,0 +1,1 @@
+from pipeline import blackjack_pipeline
---
+++ app-server/games/blackjack/pipeline.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from extract import extract
+from transform import transform
+from load import load
+
+
+def blackjack_pipeline(
+    players: Optional[int] = None,
+    games: Optional[int] = None,
+):
+
+    blackjack_response = extract(
+        games=games,
+        players=players,
+    )
+
+    data_frames = transform(blackjack_response)
+
+    load(data_frames)
