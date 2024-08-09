c8e85c0dcbabe3ee3d8bea8f523226d3de91c2fd
markcabalona <markcabalona58@gmail.com>
2024-08-09 20:29:26 +0800

    roulette-etl: update extract to return list of games
    - add games_count args
      - call /roullet endpoint [games_count] times and return response as a list of RouletteResponse

--- app-server/games/roulette/extract.py
+++ app-server/games/roulette/extract.py
@@ -1,17 +1,19 @@
 import json
+from typing import List
 from requests import get, Response
 from .models import RouletteResponse
 
 
-def extract() -> RouletteResponse:
+def extract(games_count: int = 1) -> List[RouletteResponse]:
     base_url: str = "http://127.0.0.1:8000"
     path: str = "/roulette"
 
     url = base_url + path
 
-    response: Response = get(url)
+    result: List[RouletteResponse] = []
+    for _ in range(games_count):
+        response: Response = get(url)
+        if response.ok:
+            result.append(RouletteResponse.from_json(json.dumps(response.json())))
 
-    if response.ok:
-        return RouletteResponse.from_json(json.dumps(response.json()))
-    else:
-        raise Exception(response.json())
+    return result
--- app-server/games/roulette/pipeline.py
+++ app-server/games/roulette/pipeline.py
@@ -3,9 +3,9 @@ from .transform import transform
 from .load import load
 
 
-def roulette_pipeline():
-    roulette_response = extract()
+def roulette_pipeline(games_count: int = 1):
+    roulette_response = extract(games_count)
 
-    roulette_dfs = transform(roulette=roulette_response)
+    roulette_dfs = transform(roulette=roulette_response[0])
 
     load(roulette_dfs)
