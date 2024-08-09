8a50fc04083bf48d8a7905fef70e8e32b23396d6
markcabalona <markcabalona58@gmail.com>
2024-08-09 20:24:21 +0800

    roulette-etl: invoke roulette pipeline in server.app

--- app-server/server/app.py
+++ app-server/server/app.py
@@ -3,10 +3,13 @@ import sys
 import pandas as pd
 
 # Add the parent directory of 'games' (i.e., 'app-server') to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # Import modules
-from games.baccarat import pipeline_baccarat 
+from games.roulette import roulette_pipeline
+from games.baccarat import pipeline_baccarat
 
 
 pipeline_baccarat()
+
+roulette_pipeline()
