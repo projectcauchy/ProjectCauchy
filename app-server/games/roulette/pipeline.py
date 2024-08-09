469d4f96228c9158f4d0b83aae92524d4a6f9328
markcabalona <markcabalona58@gmail.com>
2024-08-09 20:16:28 +0800

    roulette-etl: implement roulette pipeline

---
+++ app-server/games/roulette/pipeline.py
@@ -0,0 +1,11 @@
+from extract import extract
+from transform import transform
+from load import load
+
+
+def roulette_pipeline():
+    roulette_response = extract()
+
+    roulette_dfs = transform(roulette=roulette_response)
+
+    load(roulette_dfs)
