0a1e68c9ebdeec8c306ab8ba29e53576fd6b0057
projectcauchy <benjcabalonajr@gmail.com>
2024-08-11 09:35:44 +0800

    Update load.py
    write the data in root of app-server during dev

--- app-server/games/bigwheel/load.py
+++ app-server/games/bigwheel/load.py
@@ -3,4 +3,4 @@ from datetime import datetime
 
 # TODO: Write to DB for production
 def load(df: pd.DataFrame):
-    df.to_csv(f"data/bigwheel_{datetime.now()}.csv",index=False)
+    df.to_csv(f"bigwheel_{datetime.now()}.csv",index=False)
