95ee067c249fab55146981dbb4e60abd8264d763
Lumine_ <grace.estrada@ftwfoundation.org>
2024-08-12 15:40:04 +0800

    Removed utils.py in app-server/games directory

--- app-server/games/utils.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-
-
-def get_response(endpoint: str) -> json:
-    url = "http://127.0.0.1:8000/" + endpoint
-    response = requests.get(url)
-    return response.json()
